605c5abc4c767b2d2e99083e398735849dd1f25d
John-John Markstedt <John-John@Markstedts.org>
2025-10-04 13:18:04 +0200

    First java module.

---
+++ backend/jjgen/src/main/java/Main.java
@@ -0,0 +1,7 @@
+import jjfw.Dependency;
+
+public class Main {
+    public static void main(String args[]) {
+        Dependency.call();
+    }
+}
---
+++ backend/jjgen/src/main/java/jjfw/Dependency.java
@@ -0,0 +1,8 @@
+package jjfw;
+
+public class Dependency {
+
+    public static void call() {
+        System.out.println("Dependency.");
+    }
+}
